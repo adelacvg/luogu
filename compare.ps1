1983d851d774891f8048154553bf541219573012
adelacvg <adelacvgaoiro@163.com>
2021-09-20 18:29:16 +0800

    update

--- compare.ps1
+++
@@ -1,4 +0,0 @@
-CLear-Host
-$a = Get-Content "C:\Users\adela\Desktop\template_msword zh\``[Content_Types``].xml"
-$b = Get-Content "C:\Users\adela\Desktop\template_msword\``[Content_Types``].xml"
-Compare-Object $a $b -IncludeEqual
