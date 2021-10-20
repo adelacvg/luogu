512b790b1b108fc4584a141d65eb66af03d704c2
adelacvg <adelacvgaoiro@gmail.com>
2021-10-20 21:43:06 +0800

    update

--- P1450.cpp
+++ P1450.cpp
@@ -1,18 +1,20 @@
 #include<bits/stdc++.h>
 using namespace std;
+using i64 = long long;
 i64 dp[100010];
 i64 c[10],d[10];
 int n,s;
-using i64 = long long;
 int main()
 {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
     for(int i=1;i<=4;i++)
         cin>>c[i];
     cin>>n;
     dp[0]=1;
     for(i64 j=1;j<=4;j++)
     {
-        for(i64 i=c[i];i<=100010;i++)
+        for(i64 i=c[j];i<=100010;i++)
         {
             dp[i]+=dp[i-c[j]];
         }
@@ -21,20 +23,27 @@ int main()
     {
         cin>>d[1]>>d[2]>>d[3]>>d[4];
         cin>>s;
-        i64 ans=dp[s];
-        for(int j=1;j<=4;j++)
-            ans-=dp[s-(d[j]+1)*c[j]];
-        for(int j=1;j<=4;j++)
-            for(int k=j+1;k<=4;k++)
-                ans+=dp[s-(d[j]+1)*c[j]-(d[k]+1)*c[k]];
-        for(int j=1;j<=4;j++)
-            for(int k=j+1;k<=4;k++)
-                for(int l=k+1;k<=4;k++)
-                    ans-=dp[s-
-                    (d[j]+1)*c[j]-
-                    (d[k]+1)*c[k]-
-                    (d[l]+1)*c[l]];
-        ans+=
+        i64 ans=0;
+        for(int j=0;j<=((1<<4)-1);j++)
+        {
+            auto t=s;
+            int cnt=0;
+            for(int k=1;k<=4;k++)
+            {
+                if((j>>(k-1))&1)
+                {
+                    t-=c[k]*(d[k]+1);
+                    cnt^=1;
+                }
+            }
+            if(t<0)
+                continue;
+            if(!cnt)
+                ans+=dp[t];
+            else
+                ans-=dp[t];
+        }
+        cout<<ans<<'\n';
     }
 
 }
