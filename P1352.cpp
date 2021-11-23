38492593f81c470d8533e8b6a9e0f3b13706e9d5
adelacvg <adelacvgaoiro@163.com>
2021-11-23 20:51:36 +0800

    update

--- P1352.cpp
+++ P1352.cpp
@@ -1,6 +1,5 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-
 
 int a[6010];
 int fa[6010];
@@ -9,35 +8,35 @@ vector<int> e[6010];
 
 void dfs(int x)
 {
-    dp[x][0]=0;
-    dp[x][1]=a[x];
-    for(auto i:e[x])
+    dp[x][0] = 0;
+    dp[x][1] = a[x];
+    for (auto i : e[x])
     {
         dfs(i);
-        dp[x][0]+=max(dp[i][0],dp[i][1]);
-        dp[x][1]+=dp[i][0];
+        dp[x][0] += max(dp[i][0], dp[i][1]);
+        dp[x][1] += dp[i][0];
     }
 }
 
 int main()
 {
     int n;
-    cin>>n;
-    for(int i=1;i<=n;i++)
+    cin >> n;
+    for (int i = 1; i <= n; i++)
     {
-        cin>>a[i];
+        cin >> a[i];
     }
-    for(int i=1;i<n;i++)
+    for (int i = 1; i < n; i++)
     {
-        int x,y;
-        cin>>x>>y;
+        int x, y;
+        cin >> x >> y;
         e[y].emplace_back(x);
-        fa[x]=y;
+        fa[x] = y;
     }
     int root;
-    for(int i=1;i<=n;i++)
-        if(!fa[i])
-            root=i;
+    for (int i = 1; i <= n; i++)
+        if (!fa[i])
+            root = i;
     dfs(root);
-    cout<<max(dp[root][0],dp[root][1])<<endl;
+    cout << max(dp[root][0], dp[root][1]) << endl;
 }
